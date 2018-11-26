37a53386ed18c3362bbad7885c9524c16151b3ac
Mitax <mitaxshah@gmail.com>
2018-11-26 00:52:07 -0500

    helper file with helper's redirect function

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,6 +3,9 @@
 // Load Config file
 require_once 'config/config.php';
 
+// Load helpers
+require_once 'helpers/url_helper.php';
+
 // Autoload Core libraries
 spl_autoload_register(function($className){
     require_once 'libraries/' . $className . '.php';
